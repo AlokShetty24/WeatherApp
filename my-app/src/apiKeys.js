d3c0a988331dae5db2962aedabe1c1bf0fd48f3a
AlokShetty24 <alokkumarshetty160@gmail.com>
2024-06-10 21:25:58 +0530

    Initial Commit

---
+++ my-app/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "068fe97bef64fbacd90652b8d2f8d3ca",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
